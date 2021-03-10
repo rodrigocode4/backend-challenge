644483610d207db99957a96b5af5cfad362f1621
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-10 17:32:44 -0300

    refactoring mathod get

--- src/main/java/br/com/intertrack/backendchallenge/resources/VehicleResource.java
+++ src/main/java/br/com/intertrack/backendchallenge/resources/VehicleResource.java
@@ -5,19 +5,19 @@ import br.com.intertrack.backendchallenge.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/vehicles")
 public class VehicleResource {
 
     @Autowired
     private VehicleRepository vehicleRepository;
 
-    @GetMapping("/api/vehicles")
-    public List<Vehicle> getVehicle() {
+    @GetMapping(value = {"", "/"})
+    List<Vehicle> getVehicle() {
         return vehicleRepository.findAll();
     }
 
