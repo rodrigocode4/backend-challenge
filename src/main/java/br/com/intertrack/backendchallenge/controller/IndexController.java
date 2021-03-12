e2ea48aa32f21cab8ac88873c5e24e566ae7d2a9
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-12 01:28:57 -0300

    add IndexController

---
+++ src/main/java/br/com/intertrack/backendchallenge/controller/IndexController.java
@@ -0,0 +1,17 @@
+package br.com.intertrack.backendchallenge.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    RedirectView index() {
+        return new RedirectView("/api/trips");
+    }
+
+}
