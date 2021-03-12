14d85506b25aa0ce1f03e1fa0326fc1ea15cced1
Rodrigo Oliveira <rodrigocode4@gmail.com>
2021-03-12 02:13:29 -0300

    add all  Getters and Setters

--- src/main/java/br/com/intertrack/backendchallenge/model/Trip.java
+++ src/main/java/br/com/intertrack/backendchallenge/model/Trip.java
@@ -32,8 +32,55 @@ public class Trip {
         return vehicleId;
     }
 
+    public void setVehicleId(Integer vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
     public Long getInitialDateTime() {
         return initialDateTime;
     }
 
+    public void setInitialDateTime(Long initialDateTime) {
+        this.initialDateTime = initialDateTime;
+    }
+
+    public Long getFinalDateTime() {
+        return finalDateTime;
+    }
+
+    public void setFinalDateTime(Long finalDateTime) {
+        this.finalDateTime = finalDateTime;
+    }
+
+    public String getInitialAddress() {
+        return initialAddress;
+    }
+
+    public void setInitialAddress(String initialAddress) {
+        this.initialAddress = initialAddress;
+    }
+
+    public String getFinalAddress() {
+        return finalAddress;
+    }
+
+    public void setFinalAddress(String finalAddress) {
+        this.finalAddress = finalAddress;
+    }
+
+    public Integer getDistanceInMeters() {
+        return distanceInMeters;
+    }
+
+    public void setDistanceInMeters(Integer distanceInMeters) {
+        this.distanceInMeters = distanceInMeters;
+    }
+
+    public Integer getTotalTimeInMinutes() {
+        return totalTimeInMinutes;
+    }
+
+    public void setTotalTimeInMinutes(Integer totalTimeInMinutes) {
+        this.totalTimeInMinutes = totalTimeInMinutes;
+    }
 }
